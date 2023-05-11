80bcb594f82ed347040060c31a3af5c89e9f9d6d
minnnaa-p <minaproject01@gmail.com>
2023-05-11 21:20:17 +0900

    update createGroup html js
    2023-05-11
    update createGroup.html
    update createGroup.js

--- js/createGroup/createGroup.js
+++ js/createGroup/createGroup.js
@@ -74,3 +74,32 @@ $('.textAreaTd>textarea').keyup(function(){
     } 
     $('.textAreaTd>p>span').html(inputLength+'/1000');   
 });
+
+function readImage(input) {
+    // 인풋 태그에 파일이 있는 경우
+    if(input.files && input.files[0]) {
+        // 이미지 파일인지 검사 (생략)
+        // FileReader 인스턴스 생성
+        const reader = new FileReader()
+        // 이미지가 로드가 된 경우
+        reader.onload = e => {
+            const previewImage = document.getElementById("previewImage")
+            previewImage.src = e.target.result
+        }
+        // reader가 이미지 읽도록 하기
+        reader.readAsDataURL(input.files[0])
+    }
+        // 업로드한 파일 주소값 보기
+        //console.log($('#file').val());
+}
+
+function popupImgFileRm(){
+	$('#file').val('');
+    document.getElementById("previewImage").src = "./images/tmp01.jpg";
+}
+
+// input file에 change 이벤트 부여
+const inputImage = document.getElementById("file")
+inputImage.addEventListener("change", e => {
+    readImage(e.target)
+})
