3d19f8371424bc40041fcc7057f2dbff79dfc6d3
minnnaa-p <minaproject01@gmail.com>
2023-05-26 00:11:37 +0900

    Update groupMain.js
    화면 너비에 따라서 멤버 보여지는 개수 조절하게 함

--- js/gruopMain/groupMain.js
+++ js/gruopMain/groupMain.js
@@ -1,28 +1,9 @@
 $(function(){
     memImageSlidePanel();
-
-    /*
-    $('.fa-chevron-left').click(function(){
-        if(qoutient==1 && mod==0) {
-        
-        } else {
-            cnt--;
-            memImgSlide(cnt);
-        }
-    });
-
-    $('.fa-chevron-right').click(function(){
-        if(cnt==(qoutient+1)) {
-
-        }else {
-            cnt++;
-            memImgSlide(cnt);
-        }
-    });
-    */
-
-
-    
+    console.log("ani width : "+aniSlidewidth);
+    console.log("갯수: " + viewNum);
+    console.log("padding : "+ slidContpadding);
+    console.log("멤버수: "+ memNum);
     $('.fa-chevron-left').click(function(){
         if(slideIndx==0) {
 
@@ -34,25 +15,20 @@ $(function(){
     
 
     $('.fa-chevron-right').click(function(){
-        if(slideIndx==memNum) {
+        if(slideIndx>=(memNum-viewNum)) {
         } else {
             slideIndx++;
             memImgSlide(slideIndx);
         }
     });
-    
-    //
-    
+
 });
+
 let memNum=memberNum(1,50); // 소모임 멤버 받아오기 현재는 1~50 랜덤
 let slideIndx=0;    //image slide index
-let mod=memNum%5;
-let qoutient=memNum/5;
-let slideCnt=0;
-let cnt=0;
-
-let aniSlideWidth=$('.animationSlide').width();
-
+let aniSlidewidth;  //animationSlide width 값 받아오는 변수
+let viewNum;        //animationSlide 화면에 보여질 멤버 수 
+let slidContpadding; //
 
 //소모임 멤버 수 받아오는 함수
 function memberNum(min,max) {
@@ -61,69 +37,33 @@ function memberNum(min,max) {
 
 //이미지 슬라이드 패널 생성하는 함수
 function memImageSlidePanel() {
+    //let width=(memNum*125)+((memNum+2)*13);
+    aniSlidewidth=$('.animationSlide').width();
+    viewNum=Math.floor(aniSlidewidth/125);
+    slidContpadding=Math.floor((aniSlidewidth-(viewNum*125))/(viewNum*2));
+    
     $('.currentNum').html(memNum);
-    let width=(memNum*125)+((memNum+2)*13);
-    
-    /*let aniWidth=$('.animationSlide').width();
-    let _width=aniWidth/5;
-    console.log("animationSlide 너비: " +aniWidth);
-    console.log("너비: " +_width);
-    $(".memImg").css({
-        width: _width,
-        height: _width,
-    });*/
-
-    //$('.sliderPanel').css('width', _width*memNum); 
-    //console.log(141*memNum);
-    //console.log(memNum);
-    
-    //let aniWidth = $('.animationSlide').width();
-    //let modWidth = aniWidth-(125*5);
-    //let padding = modWidth/5;
-    //let width = (memNum*125)+((memNum+2)*padding);
-
-    $('.sliderPanel').css('width', width*memNum); 
-    //동적으로 바꿔줘야함ㅋㅋㅋㅋㅋㅋ
-    // .animationSlide의 width를 받아와서
-    // (한개의이미지width+패딩)*memNum;
-    // 한개의 이미지의 width는 animationSlide의 width를 받아와서 퍼센트계산
-    // 패딩도... animationSldie의 넓이를 받아와 퍼센트 계산
-    //
+    $('.sliderPanel').css('width', (125+(slidContpadding*2))*memNum);
 
     //소모임 멤버 수 만큼 멤버프로필 div생성
     for(let i=0; i<memNum; i++) {
         let item='';
-        /*$(".memImg").css({
-            width: _width,
-            height: _width,
-            borderRadius: "50%"
-        });*/
-        
         item+='<div class="slideContent">';
-
-        item+='<div class="memImg" </div><br>';        //이 부분에서 멤버 프로필 사진 받아와서 프사넣기
+        $('.slideContent').css("padding",slidContpadding);  //padding 값 세팅 
+        item+='<div class="memImg"></div><br>';        //이 부분에서 멤버 프로필 사진 받아와서 프사넣기
         item+='<span class="memName">member'+(i+1)+'</span>'; //// 여기 멤버이름
         item+='</div>';
-        /*
-        $('.slideContent').css("paddingLeft",padding);
-        $('.slideContent').css("paddingRight",padding/2);
-        */
-        //위에서 계산한 width값을 너비높이로 설정해줌, $('memImg').css('width',계싼값)
         $('.sliderPanel').append(item); //생성한 멤버 프로필 슬라이더패널에 추가하기
     }
-    memImgSlide(cnt);
+
+    memImgSlide(slideIndx);
+    
 }
 
 
 // 이미지 슬라이드 함수
-function memImgSlide(indexNum) {
-
-    console.log("cnt:" + cnt);
-    console.log("몫:" + qoutient);
-    console.log("나머지" + mod);
-    console.log("멤버수:" + memNum);
-
-    if(memNum<6) { // 총멤버수가 5이하일떄
+function memImgSlide(slideIndx) {
+    if(memNum<=viewNum) { // 총멤버수가
         $('.fa-solid').removeClass('btnActive');
         $('.btnEventPrev').css({
             cursor:'default',
@@ -135,41 +75,14 @@ function memImgSlide(indexNum) {
         });
     }
 
-    /*
-    if(((indexNum==0) && (qoutient>1))){
-        $('.fa-chevron').addClass('btnActive');
-        $('.fa-chevron-left').css({
-            cursor:'default',
-            color:'#999'
-        });
-    }
-    if((indexNum!=0)&&(indexNum<qoutient)){
+    if(slideIndx<(memNum-(viewNum-1))&&slideIndx>0) { //화살표 색상 가능하게함
         $('.fa-solid').addClass('btnActive');
         $('.sliderPanel').animate({
-            left: -(141*indexNum*5)
-        });
-    }
-    if(indexNum>=qoutient) {
-        $('.fa-chevron-right').css({
-            cursor:'default',
-            color:'#999'
-        }); 
-        $('.sliderPanel').animate({
-            left: -(141*mod)
-        });
-    }
-    */
-
-
-
-    if(slideIndx<(memNum-4)&&slideIndx>0) { //화살표 색상 가능하게함
-        $('.fa-solid').addClass('btnActive');
-        $('.sliderPanel').animate({
-            left: -(141*slideIndx)
+            left: -((125+(slidContpadding*2))*slideIndx)
         });
         
     }
-    if(slideIndx==0 && !(memNum<6)) {
+    if(slideIndx==0 && !(memNum<=viewNum)) {
         $('.fa-solid').removeClass('btnActive');
         $('.btnEventPrev').css({
             cursor:'default',
@@ -180,12 +93,12 @@ function memImgSlide(indexNum) {
             color:'#222'
         });
         $('.sliderPanel').animate({
-            left: -(141*slideIndx)
+            left: -((125+(slidContpadding*2))*slideIndx)
         });
     }
     
             
-    if(slideIndx==(memNum-5)) {
+    if(slideIndx==(memNum-viewNum)) {
         $('.fa-solid').removeClass('btnActive');
         $('.btnEventNext').css({
             cursor:'default',
@@ -196,7 +109,4 @@ function memImgSlide(indexNum) {
             color:'#222'
         });
     }
-
-    // cnt랑
-    
 }
