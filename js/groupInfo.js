f44f886c4e085378268463cdc8145cdc4c4b9a4d
gmAnn00 <bu9458@gmail.com>
2023-06-05 13:28:28 +0900

    groupMain

--- js/groupInfo.js
+++ js/groupInfo.js
@@ -1,19 +1,20 @@
-$(function(){
-
-    $(".grpLike").on("click", function() {
+$(function () {
+    $(".grpLike").on("click", function () {
         // grpLike 클래스 클릭시 ♡♥ 토글됨
         $(this).toggleClass("on");
 
         // 찜했는지 안했는지 체크하는 부분
-        if($(this).hasClass("on")) {
-            checkOnOff=true;
+        if ($(this).hasClass("on")) {
+            checkOnOff = true;
         } else {
-            checkOnOff=false;
+            checkOnOff = false;
         }
         console.log(checkOnOff);
     });
 
-    
-})
+    /*$(".joinBtn").on("click", function () {
+        alert("로그인 후 이용해주세요");
+    });*/
+});
 
-let checkOnOff=false; //찜인지 아닌지 체크하는 변수
+let checkOnOff = false; //찜인지 아닌지 체크하는 변수
