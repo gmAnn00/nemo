e16493d007ee0a4d667585cf26e391d0449ad9ab
youngjun-kim <whiteblood92@naver.com>
2023-06-09 21:19:15 +0900

    update html, css, js

--- frontend/js/myGroupMember.js
+++ frontend/js/myGroupMember.js
@@ -1,22 +1,3 @@
-$(function () {
-  // 탭
-  $(".tab-slider--body").hide();
-  $(".tab-slider--body:first").show();
-
-  $(".tab-slider--nav li").click(function() {
-      $(".tab-slider--body").hide();
-      var activeTab = $(this).attr("rel");
-      $("#"+activeTab).fadeIn();
-      if($(this).attr("rel") == "tab2"){
-          $('.tab-slider--tabs').addClass('slide');
-      }else{
-          $('.tab-slider--tabs').removeClass('slide');
-      }
-      $(".tab-slider--nav li").removeClass("active");
-      $(this).addClass("active");
-  });
-});
-
 // dot 애니메이션
 $(function () {
   $('.dot').click(function() {
