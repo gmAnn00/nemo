529b14f8ee683be5dc29c4000dfe96a0544530c3
youngjun-kim <whiteblood92@naver.com>
2023-06-23 04:18:54 +0900

    update controller

---
+++ backend/nemo/src/main/java/nemo/controller/group/GroupSettingController.java
@@ -0,0 +1,138 @@
+package com.nemo.web.group.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.nemo.web.group.dao.GroupMangerDAO;
+import com.nemo.web.group.vo.GroupVO;
+import com.nemo.web.group.vo.UserVO;
+
+//뷰와 모델 연결
+
+@WebServlet("/group/manager/setting/*")
+public class GroupSettingController extends HttpServlet{
+	HttpSession session;
+	GroupMangerDAO groupMangerDAO;
+	UserVO userVO;
+	GroupVO groupVO;
+	
+	@Override
+	public void init() throws ServletException {
+		groupMangerDAO=new GroupMangerDAO();
+		userVO=new UserVO();
+		groupVO=new GroupVO();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("컨트롤러 도착");
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out;
+		String nextPage="";
+		String action=request.getPathInfo();
+		/*String login_id = (String)session.getAttribute("user_id");*/
+		
+		System.out.println("요청 매핑이름: " + action); 
+		/*if(login_id != null) {*/
+			try {
+				
+				if(action==null||action.equals("/myGroupSetting")) {
+					
+					List<GroupVO> groupList=groupMangerDAO.groupShow();
+					List<UserVO> userList=groupMangerDAO.userShow();
+					List<UserVO> approveUserList=groupMangerDAO.approveMemberShow();
+					
+					request.setAttribute("groupList", groupList);
+					request.setAttribute("userList", userList);
+					request.setAttribute("approveUserList", approveUserList);
+					
+					nextPage="/WEB-INF/view/group/groupSetting.jsp";
+					
+				}
+				
+				//위임
+				else if(action.equals("/mandate.do")) {
+					String user_id=request.getParameter("user_id");
+					userVO.setUser_id(user_id);
+					groupMangerDAO.mandateGroupManager(userVO);
+					out = response.getWriter();
+					out.print("<script>");
+					out.print("alert('매니저를 위임했습니다');");
+						//location 포워딩 
+					out.print("location.href='" + request.getContextPath() + 
+							"/group/manager/myGroupMember.do';");
+					out.print("</script>");
+					return;
+			        
+					
+				}
+				//추방
+				else if(action.equals("/exile.do")) {
+					String user_id=request.getParameter("user_id");
+					userVO.setUser_id(user_id);
+					groupMangerDAO.exileGroupMember(userVO);
+					out = response.getWriter();
+					out.print("<script>");
+					out.print("alert('멤버를 추방했습니다');");
+					
+					out.print("location.href='" + request.getContextPath() + 
+							"/group/manager/myGroupMember.do';");
+					out.print("</script>");
+					return;
+					
+				}
+				
+				//승인
+				else if(action.equals("/approve.do")) {
+					String user_id=request.getParameter("user_id");
+					userVO.setUser_id(user_id);
+					groupMangerDAO.approveUser(userVO);
+					groupMangerDAO.approveListUpdate();
+					out = response.getWriter();
+					out.print("<script>");
+					out.print("alert('가입을 승인했습니다');");
+					
+					out.print("location.href='" + request.getContextPath() + 
+							"/group/manager/myGroupMember.do';");
+					out.print("</script>");
+					return;
+			        
+				}
+				
+				RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+				dispatcher.forward(request, response);
+	
+			
+			} catch (Exception e) {
+				System.out.println("요청 처리 중 에러!!");
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+		}
+	/*}*/
+	
+}
---
+++ backend/nemo/src/main/java/nemo/service/group/GroupMangerService.java
@@ -0,0 +1,32 @@
+package com.nemo.web.group.service;
+
+import com.nemo.web.group.dao.GroupMangerDAO;
+import com.nemo.web.group.vo.GroupVO;
+import com.nemo.web.group.vo.UserVO;
+
+//메소드
+public class GroupMangerService {
+	UserVO userVO;
+	GroupVO groupVO;
+	GroupMangerDAO groupMangerDAO;
+	
+	public GroupMangerService() {
+		groupMangerDAO=new GroupMangerDAO();
+	}
+	
+	//승인 메소드
+	
+	//거절 메소드
+	
+	//추방 메소드
+	
+	//신고 메소드
+	
+	//위임 메소드
+	
+	//소모임 정보 수정 메소드
+	
+	//게시글 삭제 메소드
+	
+	//댓글 삭제 메소드
+}
