4e6c7448777c4847581199deb71b9ba9852312bc
zxcv3214 <121008940+zxcv3214@users.noreply.github.com>
2023-06-24 20:32:29 +0900

    Add files via upload

--- backend/nemo/src/main/java/nemo/service/user/JoinService.java
+++ backend/nemo/src/main/java/nemo/service/user/JoinService.java
@@ -1,7 +1,8 @@
-package nemo.service.member;
+package nemo.service.user;
 
-import nemo.dao.member.JoinDAO;
-import nemo.vo.member.UserVO;
+import nemo.dao.user.JoinDAO;
+import nemo.vo.user.InterestsVO;
+import nemo.vo.user.UserVO;
 
 public class JoinService {
 	JoinDAO dao;
@@ -10,8 +11,11 @@ public class JoinService {
 		dao = new JoinDAO();
 	}
 	//입력한 회원정보 받기
-	public void addMember(UserVO userVO) {
-		
-		
+	public void adduser(UserVO userVO) {
+		dao.adduser(userVO);
+	}
+	//입력한 관심사 정보 받기
+	public void addChoice(InterestsVO interestsVO) {
+		dao.addChoice(interestsVO);
 	}
 }
