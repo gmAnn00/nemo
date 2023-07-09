450912ddc37be73f7de08c88671791fa494eacfe
minnnaa-p <minaproject01@gmail.com>
2023-07-09 12:23:32 +0900

    찜한 소모임 가입시 찜 삭제

--- backend/nemo/src/main/java/nemo/service/group/JoinGroupService.java
+++ backend/nemo/src/main/java/nemo/service/group/JoinGroupService.java
@@ -1,22 +1,31 @@
 package nemo.service.group;
 
+import nemo.dao.group.BookmarkDAO;
 import nemo.dao.group.JoinGroupDAO;
 
 public class JoinGroupService {
 	JoinGroupDAO joinGroupDAO;
+	BookmarkDAO bookmarkDAO;
 	
 	public JoinGroupService() {
 		joinGroupDAO = new JoinGroupDAO();
+		bookmarkDAO = new BookmarkDAO();
 	}
 
 	public boolean joinGroup(String user_id, int group_id) {
 		boolean result = false;
 		boolean wait = false;
+		boolean bookmark = false;
 		
+		bookmark= bookmarkDAO.isBookmark(user_id, group_id);
 		wait = joinGroupDAO.isWait(group_id);
+		//bookmark에 있는지 확인
 		
 		if(wait == false) {
 			result = joinGroupDAO.joinGroup(user_id, group_id);
+			if(bookmark) {
+				bookmarkDAO.deleteBookmark(user_id, group_id);
+			}
 		}else {
 			result = joinGroupDAO.joinWaitList(user_id, group_id);
 		}
