92f1813985050cb7fd4af8ba1e5502561ea242b2
gmAnn00 <bu9458@gmail.com>
2023-07-10 17:09:52 +0900

    Update CreateGroupController.java

--- backend/nemo/src/main/java/nemo/controller/group/CreateGroupController.java
+++ backend/nemo/src/main/java/nemo/controller/group/CreateGroupController.java
@@ -2,6 +2,7 @@ package nemo.controller.group;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
