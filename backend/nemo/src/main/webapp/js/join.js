474eec8248f7baac11851eb90ccfcbf4cf594da1
minnnaa-p <minaproject01@gmail.com>
2023-07-10 17:15:31 +0900

    회원가입 유효성 체크

--- backend/nemo/src/main/webapp/js/join.js
+++ backend/nemo/src/main/webapp/js/join.js
@@ -1,3 +1,9 @@
+let idCheck=false;
+let pwdCheck=false;
+let allPwdCheck=false;
+let nickCheck=false;
+let phoneCheck=false;
+
 $(document).ready(function() {
 	$("#domainList").on("change", function() {
 		var emailDomain = $("#emailDomain");
@@ -10,7 +16,9 @@ $(document).ready(function() {
 		}
 	});
 
-
+	$("#alertSuccess").hide();
+	$("#alertDanger").hide();
+/*
 	//비밀번호 확인
 	$("#alertSuccess").hide();
 	$("#alertDanger").hide();
@@ -28,14 +36,16 @@ $(document).ready(function() {
 				$("#submit").attr("disabled", "disabled");
 			}
 		}
-	}); //end 비밀번호 확인
+	}); //end 비밀번호 확인*/
 
 });
 
 
 //아이디 중복 체크
 function fn_IdCheck() {
+	idCheck=false;
 	let user_idVO="";
+	let idRegExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_-]{5,20}$/;
 	if($("#user_id_hidden").val() != null){
 		user_idVO = $("#user_id_hidden").val();
 
@@ -45,46 +55,129 @@ function fn_IdCheck() {
 	let user_id = $("#user_id").val();
 	
 	//입력 안했을 경우
-	if (user_id == "") {
+	if (user_id.length<5) {
 		$("#resultMsgId").show();
-		$("#resultMsgId").html("중복 체크할 아이디을 입력해주세요");
+		$("#resultMsgId").html("중복 체크할 아이디를 5글자 이상으로 입력해주세요");
 		$("#resultMsgId").css("color", "#f43965");
-		return; //아래 내용 수행안하고 위로 돌아감
-
-	}
-
-	if (user_id == user_idVO) {
+		return;
+	} else if(user_id.length>=5 || user_id.length<=20) {
+		if(!idRegExp.test(user_id)){
+			if(user_id.length>20) {
+				$("#resultMsgId").show();
+				$("#resultMsgId").html("중복 체크할 아이디를 20자 이하로 입력해주세요");
+				$("#resultMsgId").css("color", "#f43965");
+				return;
+			} else {		
+				$("#resultMsgId").show();
+				$("#resultMsgId").html("중복 체크할 아이디를 형식에 맞게 입력해주세요");
+				$("#resultMsgId").css("color", "#f43965");
+				return;
+			}
+		} else {
+			$.ajax({
+				type: "post",
+				async: true,
+				// dataType: "text",
+				url: "/nemo/duplicate/id",
+				//왼쪽의 key에 오른쪽의 value값이 들어감
+				data: { "user_id": user_id },
+				success: function(data, textStatus) {
+					if (data == "usable") {
+						idCheck=true;
+						$("#resultMsgId").show();
+						$("#resultMsgId").html("사용할 수 있는 아이디입니다.");
+						$("#resultMsgId").css("color", "var(--blue-color)");
+					} else {
+						$("#resultMsgId").show();
+						$("#resultMsgId").html("사용할 수 없는 아이디입니다.");
+						$("#resultMsgId").css("color", "#f43965");
+					}
+				},
+				error: function(data, textStatus, error) {
+					alert("에러가 발생했습니다.");
+				},
+			});
+		}
+	} 
+	/*if (user_id == user_idVO) {
 		$("#resultMsgId").show();
 		$("#resultMsgId").html("지금 사용하고 있는 아이디입니다.");
 		$("#resultMsgId").css("color", "#3384ff");
-	} else {
-		$.ajax({
-			type: "post",
-			async: true,
-			// dataType: "text",
-			url: "/nemo/duplicate/id",
-			//왼쪽의 key에 오른쪽의 value값이 들어감
-			data: { "user_id": user_id },
-			success: function(data, textStatus) {
-				if (data == "usable") {
-					$("#resultMsgId").show();
-					$("#resultMsgId").html("사용할 수 있는 아이디입니다.");
-					$("#resultMsgId").css("color", "var(--blue-color)");
-				} else {
-					$("#resultMsgId").show();
-					$("#resultMsgId").html("사용할 수 없는 아이디입니다.");
-					$("#resultMsgId").css("color", "#f43965");
-				}
-			},
-			error: function(data, textStatus, error) {
-				alert("에러가 발생했습니다.");
-			},
-		});
+	}*/ 
+}
+
+//비밀번호 유효성 체크
+function fn_pwdCheck() {
+	allPwdCheck=false;
+	pwdCheck=false;
+	let password = $("#password").val();
+	let num = password.search(/[0-9]/g);
+	let eng = password.search(/[a-z]/ig);
+	//let special = password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+	let special = password.search(/[`\-=\\\[\];',\./~!@#\$%\^&\*\(\)_\+|\{\}:"<>\?]/gi);
+	let pwdRegExp=/^[A-Za-z0-9`\-=\\\[\];',\./~!@#\$%\^&\*\(\)_\+|\{\}:"<>\?]{8,20}$/;
+	
+	let passwordCheck = $("#passwordCheck").val();
+	if(passwordCheck !='' || !(passwordCheck.length<=0)) {
+		if (password == passwordCheck) {
+			allPwdCheck=true;
+			$("#alertSuccess").show();
+			$("#alertDanger").hide();
+			$("#submit").removeAttr("disabled");
+		} else {
+			$("#alertSuccess").hide();
+			$("#alertDanger").show();
+			$("#submit").attr("disabled", "disabled");
+		}
 	}
+	if(password.length<8||password.length>20) {
+		$("#resultMsgPwd").show();
+		$("#resultMsgPwd").html("비밀번호를 8~20자리 이내로 입력해주세요.");
+		$("#resultMsgPwd").css("color", "#f43965");
+		return;
+	} else if(password.search(/\s/)!=-1){
+		$("#resultMsgPwd").show();
+		$("#resultMsgPwd").html("비밀번호를 공백 없이 입력해주세요.");
+		$("#resultMsgPwd").css("color", "#f43965");
+		return;
+	} else if(!pwdRegExp.test(password)) {
+		$("#resultMsgPwd").show();
+		$("#resultMsgPwd").html("비밀번호를 문자, 숫자, 특수문자포함 8~20자로 입력해주세요.");
+		$("#resultMsgPwd").css("color", "#f43965");
+		return;
+	} else {
+		pwdCheck=true;
+		$("#resultMsgPwd").hide();
+		return;
+	}
+	//
+	
+}
+
+function fn_pwdDupCheck() {
+	allPwdCheck=false;	
+	$("#passwordCheck").keyup(function() {
+		let password = $("#password").val();
+		let passwordCheck = $("#passwordCheck").val();
+		if (password != "" || passwordCheck != "") {
+			if (password == passwordCheck) {
+				allPwdCheck=true;
+				$("#alertSuccess").show();
+				$("#alertDanger").hide();
+				$("#submit").removeAttr("disabled");
+			} else {
+				$("#alertSuccess").hide();
+				$("#alertDanger").show();
+				$("#submit").attr("disabled", "disabled");
+			}
+		}
+	});
+	
 }
 
 //닉네임 중복 체크
 function fn_nicknameCheck() {
+	nickCheck=false;
 	let nicknameVO = $("#nickname_hidden").val();
 	console.log("nickname=" + nicknameVO);
 
@@ -111,6 +204,7 @@ function fn_nicknameCheck() {
 			data: { "nickname": nickname },
 			success: function(data, textStatus) {
 				if (data == "usable") {
+					nickCheck=true;
 					$("#resultMsgNick").show();
 					$("#resultMsgNick").html("사용할 수 있는 닉네임입니다.");
 					$("#resultMsgNick").css("color", "#3384ff");
@@ -127,6 +221,19 @@ function fn_nicknameCheck() {
 	}
 }
 
+function fn_phoneCheck() {
+	phoneCheck=false;
+	let phone=$('#phone').val();
+	let phoneRegCheck=/^([0-9]{2,3})([0-9]{3,4})([0-9]{4})$/;
+	if(!phoneRegCheck.test(phone)){
+		$("#resultMsgPhone").show();
+		$("#resultMsgPhone").html("전화번호 형식이 맞지 않습니다. 예)01012345678");
+		$("#resultMsgPhone").css("color", "#f43965");
+	} else {
+		$("#resultMsgPhone").hide();
+		phoneCheck=true;
+	}
+}
 //이메일 중복체크
 function fn_emailCheck() {
 	let emailIdVO = $("#emailId_hidden").val();
@@ -176,81 +283,8 @@ function fn_emailCheck() {
 }
 
 function fnJoin(){
-	
-	/*
-	let user_id=$("#user_id").val();
-	let password=$("#password").val();
-	let passwordCheck=$("#passwordCheck").val();
-	let name=$("#name").val();
-	let nickname=$("#nickname").val();
-	let zipcode=$("#zipcode").val();
-	let birthdate=$("#birthdate").val();
-	let phone=$("#phone").val();
-	
-	let emailReg= /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-	let emailId=$("#emailId").val();
-	let emailDomain=$("#emailDomain").val();
-	let email="";
-	
-	if(!user_id){
-		alert("ID를 입력해주세요");
-		$("#user_id").focus();
-		return false;
-	}
-	if(!password){
-		alert("비밀번호를 입력해주세요");
-		$("#password").focus();
-		return false;
-	}
-	if(!passwordCheck){
-		alert("비밀번호 확인을 입력해주세요");
-		$("#passwordCheck").focus();
-		return false;
-	}
-	if(!nickname){
-		alert("닉네임을 입력해주세요");
-		$("#nickname").focus();
-		return false;
-	}
-	if(!zipcode){
-		alert("우편번호를 입력해주세요");
-		$("#findZipcode").focus();
-		return false;
-	}
-	if(!birthdate){
-		alert("생년월일 입력해주세요");
-		$("#birthdate").focus();
-		return false;
-	}
-	if(!phone){
-		alert("전화번호를 입력해주세요");
-		$("#phone").focus();
-		return false;
-	}
-	if(!emailId){
-		alert("이메일을 입력해주세요");
-		$("#emailId").focus();
-		return false;
-	}
-	if(!emailDomain){
-		alert("이메일 도메인을 입력해주세요");
-		$("#emailDomain").focus();
-		return false;
-	}
-	mail= emailId+"@"+emailDomain;
-	
-	if(!emailReg.test(mail)){
-		alert("이메일을 형식에 맞게 입력해주세요");
-		$("#emailDomain").focus();
-		return false;
-	}
-	
-	document.getElementById('frm').submit();*/
-	
-	let isEmpty=false;
-	
+	let isEmpty=false;	
 	$('#frm').find('input[id!="user_addr2"]').each(function(){
-		
 		$(this).removeClass('isEmpty');
 		if(!$(this).val()){
 			isEmpty=true;
@@ -266,6 +300,27 @@ function fnJoin(){
 		let emailId=$("#emailId").val();
 		let emailDomain=$("#emailDomain").val();
 		let email="";
+		
+		if(!idCheck) {
+			alert('ID를 다시 입력해주세요');
+			$("#user_id").focus();
+			return false;
+		}
+		if(!pwdCheck||!allPwdCheck) {
+			alert('비밀번호를 다시 입력해주세요');
+			$("#password").focus();
+			return false;
+		}
+		if(!nickCheck) {
+			alert('닉네임을 다시 입력해주세요');
+			$("#nickname").focus();
+			return false;
+		}
+		if(!phoneCheck) {
+			alert('전화번호를 다시 입력해주세요');
+			$('#phone').focus();
+			return false;
+		}
 		mail= emailId+"@"+emailDomain;
 	
 		$('#emailId').removeClass('isEmpty');
